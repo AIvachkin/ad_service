3b95e0c5481292e98d4aae619ea5568a397e21f1
andre <Andrews.ru83@gmail.com>
2023-06-14 17:03:17 +0300

    ad_service_dev

---
+++ src/main/java/ru/skypro/homework/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package ru.skypro.homework;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableAutoConfiguration
+public class WebSecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v3/api-docs",
+            "/webjars/**",
+            "/users/me/image",
+            "/users/avatar/*",
+            "/image/**",
+            "/avatar/**",
+            "/ads",
+            "/login", "/register"
+    };
+
+    @Bean
+    public JdbcUserDetailsManager userDetailsService(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests((authz) ->
+                        authz
+                                .mvcMatchers(AUTH_WHITELIST).permitAll()
+                                .mvcMatchers("/ads/**", "/users/**").hasRole("USER")
+                                .mvcMatchers("/ads/**", "/users/**").hasRole("ADMIN")
+
+                )
+                .cors().and()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
---
+++ src/main/java/ru/skypro/homework/service/impl/AuthServiceImpl.java
@@ -0,0 +1,74 @@
+package ru.skypro.homework.service.impl;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.RegisterReq;
+import ru.skypro.homework.dto.Role;
+import ru.skypro.homework.model.Users;
+import ru.skypro.homework.repository.UserRepository;
+import ru.skypro.homework.service.AuthService;
+
+import java.time.Instant;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private final UserDetailsManager manager;
+
+    private final PasswordEncoder encoder;
+    private final UserRepository userRepository;
+
+    public AuthServiceImpl(UserDetailsManager manager, UserRepository userRepository) {
+        this.manager = manager;
+        this.userRepository = userRepository;
+        this.encoder = new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Метод для аутентификации пользователя
+     * @param userName
+     * @param password
+     * @return
+     */
+    @Override
+    public boolean login(String userName, String password) {
+        if (!manager.userExists(userName)) {
+            return false;
+        }
+        UserDetails userDetails = manager.loadUserByUsername(userName);
+        String encryptedPassword = userDetails.getPassword();
+        String encryptedPasswordWithoutEncryptionType = encryptedPassword.substring(8);
+        return encoder.matches(password, encryptedPasswordWithoutEncryptionType);
+    }
+
+    /**
+     * Метод для регистрации пользователя
+     * @param registerReq
+     * @param role
+     * @return
+     */
+    @Override
+    public boolean register(RegisterReq registerReq, Role role) {
+        if (manager.userExists(registerReq.getUsername())) {
+            return false;
+        }
+        manager.createUser(
+                User.withDefaultPasswordEncoder()
+                        .password(registerReq.getPassword())
+                        .username(registerReq.getUsername())
+                        .roles(role.name())
+                        .build()
+        );
+        Users user = userRepository.findByEmail(registerReq.getUsername());
+        user.setFirstName(registerReq.getFirstName());
+        user.setLastName(registerReq.getLastName());
+        user.setPhone(registerReq.getPhone());
+        user.setRegDate(Instant.now());
+        userRepository.save(user);
+        return true;
+    }
+}
